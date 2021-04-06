975ac300fae053934d94706ed8f6308bb3e573a1
DS-DOJO <rpandey1901@gmail.com>
2021-04-06 18:01:17 +0200

    message updated

--- app.py
+++ app.py
@@ -3,24 +3,28 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open("model.pkl", "rb"))
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/predict',methods=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def predict():
-    '''
+    """
     For rendering results on HTML GUI
-    '''
+    """
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template(
+        "index.html", prediction_text="Predicted Salary $ {}".format(output)
+    )
 
 
 if __name__ == "__main__":
