79ad8ba805d006dbc0b8894103cec4f07c7a1299
DS-DOJO <rpandey1901@gmail.com>
2021-04-07 20:14:44 +0200

    final deployment

--- model.py
+++ model.py
@@ -1,39 +1,55 @@
-# Importing the libraries
+# import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
+from sklearn.linear_model import LinearRegression
 
-dataset = pd.read_csv('hiring.csv')
 
-dataset['experience'].fillna(0, inplace=True)
+# import data
+dataset = pd.read_csv("hiring.csv")
 
-dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
+# fill experience null values with 0
+dataset["experience"].fillna(0, inplace=True)
 
+# fill test score null value with mean
+dataset["test_score"].fillna(dataset["test_score"].mean(), inplace=True)
+
+# Cretae input feature
 X = dataset.iloc[:, :3]
 
-#Converting words to integer values
-def convert_to_int(word):
-    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
-                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
+# Converting categories to integer values
+def convert_to_numeric(word):
+    word_dict = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        "ten": 10,
+        "eleven": 11,
+        "twelve": 12,
+        "zero": 0,
+        0: 0,
+    }
     return word_dict[word]
 
-X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
 
+X["experience"] = X["experience"].apply(lambda x: convert_to_numeric(x))
+
+# target value
 y = dataset.iloc[:, -1]
 
-#Splitting Training and Test Set
-#Since we have a very small dataset, we will train our model with all availabe data.
 
-from sklearn.linear_model import LinearRegression
+# Linear Regression model
 regressor = LinearRegression()
 
-#Fitting model with trainig data
+# Fitting model with trainig data
 regressor.fit(X, y)
 
 # Saving model to disk
-pickle.dump(regressor, open('model.pkl','wb'))
-
-# Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[2, 9, 6]]))
+pickle.dump(regressor, open("model.pkl", "wb"))
